805811c1e09e745f4c0086d23a881222112c3382
Jasim Khamis <jkhamis@6-pence.com>
2025-02-19 17:48:00 +0300

    fixes

--- controllers/invitation.js
+++ controllers/invitation.js
@@ -32,36 +32,34 @@ router.post('/createInvite', async (req, res) => {
   try {
     const guestInDatabase = await Invitation.findOne({ email: req.body.email, eventID: req.body.eventID})
     if (guestInDatabase) {
-      return res.status(500).json({ error: 'Guest already Invited.' })
+      return res.status(401).json({ error: 'Guest already Invited.' })
     }
+    
     const createInvite = await Invitation.create(req.body)
     try {
       const eventObj = await Event.findById(req.body.eventID).populate('eventManager').populate('representatives').populate('venue')
       const to = req.body.email
       const subject = 'Invitation Email'
       const htmlContent = `
-      <h1>Hi ${req.body.guestName},</h1>
-      <p>${eventObj.description}</p>
-      <p><strong>Date:</strong> ${eventObj.date}</p>
-      <p><strong>Location:</strong> ${eventObj.addressLine}</p>
-      <p>Please let us know if you can make it by clicking the RSVP button below:</p>
-      <p>
-        <a href="${process.env.URL}/RSVP/${createInvite._id}/1" style="display:inline-block;padding:10px 20px;font-size:16px;color:#fff;background-color:#4CAF50;text-decoration:none;border-radius:5px;">Accept</a>
-      </p>
-            <p>
-        <a href="${process.env.URL}/RSVP/${createInvite._id}/0" style="display:inline-block;padding:10px 20px;font-size:16px;color:#fff;background-color:#cb1a55;text-decoration:none;border-radius:5px;">Reject</a>
-      </p>
-      <p>We look forward to seeing you there!</p>
-      <p>Best regards,<br/>${eventObj.fullName}</p>
-      `
+        <h1>Hi ${req.body.guestName},</h1>
+        <p>${eventObj.description}</p>
+        <p><strong>Date:</strong> ${eventObj.date}</p>
+        <p><strong>Location:</strong> ${eventObj.addressLine}</p>
+        <p>Please let us know if you can make it by clicking the RSVP button below:</p>
+        <p>
+          <a href="${process.env.URL}/RSVP/${createInvite._id}/1" style="display:inline-block;padding:10px 20px;font-size:16px;color:#fff;background-color:#4CAF50;text-decoration:none;border-radius:5px;">Accept</a>
+        </p>
+              <p>
+          <a href="${process.env.URL}/RSVP/${createInvite._id}/0" style="display:inline-block;padding:10px 20px;font-size:16px;color:#fff;background-color:#cb1a55;text-decoration:none;border-radius:5px;">Reject</a>
+        </p>
+        <p>We look forward to seeing you there!</p>
+        <p>Best regards,<br/>${eventObj.fullName}</p>
+        `
+
       await sendInvitationEmail(to, subject, htmlContent)
-      await Invitation.findByIdAndUpdate(createInvite._id, {
-        invitationStatus: 1
-      })
+      await Invitation.findByIdAndUpdate(createInvite._id, {invitationStatus: 'Sent'})
     } catch (error) {
-      await Invitation.findByIdAndUpdate(createInvite._id, {
-        invitationStatus: 2
-      })
+        await Invitation.findByIdAndUpdate(createInvite._id, {invitationStatus: 'Failed'})
     }
     res.status(200).json({ done: 'done' })
   } catch (error) {
